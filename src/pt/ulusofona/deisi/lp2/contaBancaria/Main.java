46c04832134dc05c4e1f030c81d608f95690a78f
a22207337_TiagoMota <a22207337@alunos.ulht.pt>
2023-10-18 20:12:49 +0100

    Adicionei "Estou no Git" na classe Main

--- src/pt/ulusofona/deisi/lp2/contaBancaria/Main.java
+++ src/pt/ulusofona/deisi/lp2/contaBancaria/Main.java
@@ -2,6 +2,6 @@ package pt.ulusofona.deisi.lp2.contaBancaria;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Estou no Git");
     }
 }
